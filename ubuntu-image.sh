90e9b8349dc8b0c94d2a40ba7fe6fd50584e729b
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-04 20:17:50 +0900

    Remove Mesa and Vulkan setup from ubuntu-image.sh
    Removed installation and setup commands for Mesa and related Vulkan drivers in the chroot environment.

--- ubuntu-image.sh
+++ ubuntu-image.sh
@@ -54,53 +54,6 @@ teardown_mountpoint() {
         umount "$submount"
     done
 }
-mount --bind /dev  "build/chroot/dev"
-mount --bind /proc "build/chroot/proc"
-mount --bind /sys  "build/chroot/sys"
-# Mesa new part3
-chroot build/chroot apt-get -y install build-essential meson ninja-build pkgconf pkgconf-bin python3-mako \
-  libdrm-dev libpciaccess-dev libffi-dev libsensors-dev libxml2-dev \
-  libx11-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-glx0-dev \
-  libxcb-present-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-xfixes0-dev libxcb1-dev \
-  libxdmcp-dev libxext-dev libxrandr-dev libxrender-dev libxshmfence-dev libxxf86vm-dev \
-  libwayland-dev libwayland-bin libwayland-egl-backend-dev wayland-protocols \
-  libglvnd-core-dev libvulkan-dev glslang-tools spirv-tools spirv-tools-dev \
-libclc-21-dev llvm-21-dev libllvmspirvlib-21-dev libclang-cpp21-dev libclang-21-dev \
-git
-chroot build/chroot /bin/bash -c "apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-tools clapper mpv vulkan-tools mesa-utils"
-# mesa
-mkdir build/chroot/bbb
-chroot build/chroot /bin/bash -c "cd bbb && git clone --depth 1 https://gitlab.freedesktop.org/mesa/libdrm && cd libdrm/ && mkdir build && cd build/ && meson && ninja install"
-#chroot build/chroot /bin/bash -c "cd bbb && git clone --depth 1 -b staging/25.3 https://gitlab.freedesktop.org/mesa/mesa.git && cd mesa && mkdir build && cd build && meson -Dvulkan-drivers=panfrost -Dgallium-drivers=panfrost -Dlibunwind=false -Dprefix=/opt/panfrost && ninja install && echo /opt/panfrost/lib/aarch64-linux-gnu | tee /etc/ld.so.conf.d/0-panfrost.conf && echo 'VK_DRIVER_FILES="/opt/panfrost/share/vulkan/icd.d/panfrost_icd.aarch64.json"' >> /etc/environment"
-
-# Mesaの仕入れとビルド（Panthor最適化版）
-chroot build/chroot /bin/bash -c "cd bbb && git clone --depth 1 -b staging/25.3 https://gitlab.freedesktop.org/mesa/mesa.git && cd mesa && mkdir build && cd build && meson setup .. -Dvulkan-drivers=panfrost -Dgallium-drivers=panfrost -Dplatforms=x11,wayland -Dbuildtype=release -Dprefix=/opt/panthor && ninja install"
-# -Dlibunwind=false
-
-# 共有ライブラリのパスを通す
-chroot build/chroot /bin/bash -c "echo /opt/panthor/lib/aarch64-linux-gnu | tee /etc/ld.so.conf.d/0-panthor.conf && ldconfig"
-
-# Vulkanドライバーの環境変数を定義
-chroot build/chroot /bin/bash -c "echo 'VK_DRIVER_FILES=\"/opt/panthor/share/vulkan/icd.d/panfrost_icd.aarch64.json\"' >> /etc/environment"
-
-chroot build/chroot /bin/bash -c "cat << 'EOF' > /etc/profile.d/rockchip-panthor.sh
-# 1. 明示的に新グラフィックドライバ（Panthor）をロードする指示
-# export MESA_LOADER_DRIVER_OVERRIDE=panthor
-
-# 2. FirefoxをWayland（GPUアクセラレーション必須環境）で動かす設定
-export MOZ_ENABLE_WAYLAND=1
-
-# 3. Chromium/Chrome系列でPanthor GPUを強制認識させるフラグ
-export CHROMIUM_FLAGS=\"--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-gpu-rasterization --enable-zero-copy\"
-EOF
-chmod +x /etc/profile.d/rockchip-panthor.sh"
-
-umount "build/chroot/sys"
-umount "build/chroot/proc"
-umount "build/chroot/dev"
-rm -rf build/chroot/bbb
-
-
 
 	setup_mountpoint build/chroot
 	 mkdir build/chroot/kernel
