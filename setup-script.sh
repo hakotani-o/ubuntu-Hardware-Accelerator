3a49369a0168bd22be0f8ce0746a1969a26b6447
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-06 07:26:14 +0900

    Update GStreamer package installation in setup script

--- setup-script.sh
+++ setup-script.sh
@@ -20,7 +20,7 @@ EOF
 	chown syslog:adm /var/log/syslog
 	ssh-keygen -A
 	
-	apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-tools clapper mpv vulkan-tools mesa-utils
+	apt-get install -y gstreamer1.0-plugins-good gstreamer1.0-plugins-base gstreamer1.0-gl clapper mpv vulkan-tools mesa-utils
 	dpkg -i kernel/*
 	apt-get install -f -y
 	cd / && rm -rf kernel
