5d68b3172af1ea7b95c0b244c81ed322c8c73676
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-07 15:13:12 +0900

    Update meas-build-env.sh

--- meas-build-env.sh
+++ meas-build-env.sh
@@ -8,12 +8,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-kernel=`ls *.deb|wc -l`
-if [ $kernel -ne 3 ]; then
-	echo "Build kernel first"
-	exit 1
-fi
-
 setup_mountpoint() {
     local mountpoint="$1"
 
