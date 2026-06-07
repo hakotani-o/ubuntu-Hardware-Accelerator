bc607ab5f3e90494aacc2c2912ac8b0e568c7cf8
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-07 20:09:06 +0900

    Fix unmount command to use chroot_dir variable

--- meas-build-env.sh
+++ meas-build-env.sh
@@ -135,7 +135,8 @@ cp build_panthor_mesa.sh $1 && chmod +x $1/build_panthor_mesa.sh
 chroot $1 /build_panthor_mesa.sh
 cp $1/*.deb .
 ls *.deb
+teardown_mountpoint $chroot_dir
 if [ $mem_size -gt 10 ]; then
-	umount $1
+	umount $chroot_dir
 	sleep 2
 fi
