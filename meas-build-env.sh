4a946ecf12081f150e754c96ad44c24cb29bad14
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-07 15:17:16 +0900

    Update meas-build-env.sh

--- meas-build-env.sh
+++ meas-build-env.sh
@@ -7,6 +7,15 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "Please run as root"
     exit 1
 fi
+# ディスクイメージを作成するために必要なツールをインストール
+sudo apt-get update && sudo apt-get -y install  build-essential gcc-aarch64-linux-gnu bison \
+qemu-user-static qemu-system-arm qemu-efi-aarch64 binfmt-support \
+debootstrap flex libssl-dev bc rsync kmod cpio xz-utils fakeroot parted \
+udev dosfstools uuid-runtime git-lfs device-tree-compiler python3 \
+python-is-python3 fdisk bc debhelper python3-pyelftools python3-setuptools \
+python3-pkg-resources swig libfdt-dev libpython3-dev gawk \
+git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex \
+libelf-dev bison libgnutls28-dev libdw-dev
 
 setup_mountpoint() {
     local mountpoint="$1"
