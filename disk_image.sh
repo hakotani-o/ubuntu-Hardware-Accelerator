7d30956410ff714f9396f686f90917ebe3afb014
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-08 09:33:11 +0900

    Remove automatic expansion script from disk_image.sh

--- disk_image.sh
+++ disk_image.sh
@@ -223,13 +223,7 @@ losetup -d "${loop}"
 
 # Exit trap is no longer needed
 trap '' EXIT
-# ==================== ★ここに自動拡張スクリプトを仕込む追加コード ====================
-if [ ! -f pishrink.sh ]; then
-    wget -q https://githubusercontent.com
-fi
-chmod +x pishrink.sh
-sudo ./pishrink.sh "${img}"  # これを実行するだけで自動拡張機能が内部に組み込まれます
-# ====================================================================================
+
 echo -e "\nCompressing $(basename "${img}.xz")\n"
 xz -v -9 -T0 "${img}"
 #rm "${img}"
