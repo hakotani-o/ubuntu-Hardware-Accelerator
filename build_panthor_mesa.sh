a606eab8e4ca6b4d6840fb745cc5412e64286ede
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-06-27 10:08:23 +0900

    Update build_panthor_mesa.sh

--- build_panthor_mesa.sh
+++ build_panthor_mesa.sh
@@ -74,6 +74,8 @@ sudo apt-get install -y build-essential devscripts debhelper ninja-build \
 # ソースのダウンロード
 apt source mesa
 MESA_SRC_DIR=$(ls -d mesa-*)
+mesa_version=$( echo $MESA_SRC_DIR | sed 's/mesa-//' )
+echo "$mesa_version" > rel.txt
 cd "$MESA_SRC_DIR"
 
 ### === 【追加】debian/changelog の自動書き換え ===
